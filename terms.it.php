0059f929a645e91fc121c7a6d18851b5b091f23e
MelonBoiDev <marcello.ferrara03@gmail.com>
2023-07-30 15:16:54 +0200

    uploading

---
+++ terms.it.php
@@ -0,0 +1,78 @@
+<?php 
+
+$title = 'Termini di Servizio';
+$date = 'Ultimo aggiornamento: 27 luglio 2023';
+
+$text_intro = 'Si prega di leggere attentamente queste condizioni prima di utilizzare il nostro servizio.';
+
+$interpretation_definition = 'Interpretazione e definizioni';
+$interpretation = 'Interpretazione';
+$interpretation_text = 'Le parole il cui iniziale è maiuscola hanno significati definiti nelle seguenti condizioni. Le seguenti definizioni avranno lo stesso significato, indipendentemente che appaiano al singolare o al plurale.';
+$definition = 'Definizioni';
+$definition_text = 'Ai fini di queste Termini e Condizioni:';
+$def_affiliate = '<strong>Affiliato</strong> indica un ente che controlla, è controllato da o è sotto il controllo comune di una parte, dove "controllo" significa il possesso del 50% o più delle azioni, interessi patrimoniali o altri titoli aventi diritto di voto per l\'elezione dei direttori o di un altro organo amministrativo.';
+$def_country = '<strong>Paese</strong> si riferisce a: Italia';
+$def_company = '<strong>Azienda</strong> (indicata come "l\'Azienda", "Noi", "Il nostro" in questo Accordo) si riferisce a CodeLiftoff.';
+$def_device = '<strong>Dispositivo</strong> indica qualsiasi dispositivo in grado di accedere al Servizio, come un computer, un cellulare o un tablet digitale.';
+$dev_service = '<strong>Servizio</strong> si riferisce al Sito Web.';
+$def_term_cond = '<strong>Termini e Condizioni</strong> (anche indicati come "Termini") significano questi Termini e Condizioni che costituiscono l\'intero accordo tra Te e l\'Azienda riguardo all\'utilizzo del Servizio. Questo accordo Termini e Condizioni è stato creato con l\'aiuto del <a href="https://www.freeprivacypolicy.com/free-terms-and-conditions-generator/" target="_blank">Generatore gratuito di Termini e Condizioni</a>.';
+$def_social = '<strong>Servizio di Social Media di Terze Parti</strong> indica qualsiasi servizio o contenuto (incluse informazioni, dati, prodotti o servizi) fornito da un terzo che può essere visualizzato, incluso o reso disponibile tramite il Servizio.';
+$def_website = '<strong>Sito Web</strong> si riferisce a CodeLiftoff, accessibile da <a href="wwwcodeliftoff.com" rel="external nofollow noopener" target="_blank">wwwcodeliftoff.com</a>.';
+$def_you = '<strong>Tu</strong> indica l\'individuo che accede o utilizza il Servizio o l\'azienda o altra entità legale per conto della quale tale individuo accede o utilizza il Servizio, a seconda del caso.';
+
+$acknowledgement = 'Riconoscimento';
+$acknowledgement_text1 = 'Questi sono i Termini e Condizioni che regolano l\'utilizzo di questo Servizio e l\'accordo tra Te e l\'Azienda. Questi Termini e Condizioni stabiliscono i diritti e gli obblighi di tutti gli utenti riguardo l\'utilizzo del Servizio.';
+$acknowledgement_text2 = 'Il tuo accesso e utilizzo del Servizio sono condizionati dalla tua accettazione e conformità a questi Termini e Condizioni. Questi Termini e Condizioni si applicano a tutti i visitatori, utenti e altre persone che accedono o utilizzano il Servizio.';
+$acknowledgement_text3 = 'Accedendo o utilizzando il Servizio, accetti di essere vincolato da questi Termini e Condizioni. Se non sei d\'accordo con qualsiasi parte di questi Termini e Condizioni, non potrai accedere al Servizio.';
+$acknowledgement_text4 = 'Dichiari di avere più di 18 anni. L\'Azienda non permette ai minori di 18 anni di utilizzare il Servizio.';
+$acknowledgement_text5 = 'Il tuo accesso e utilizzo del Servizio sono anche condizionati dalla tua accettazione e conformità alla Politica sulla Privacy dell\'Azienda. La nostra Politica sulla Privacy descrive le nostre politiche e procedure riguardanti la raccolta, l\'utilizzo e la divulgazione delle tue informazioni personali quando utilizzi l\'Applicazione o il Sito Web e ti informa dei tuoi diritti sulla privacy e di come la legge ti protegge. Si prega di leggere attentamente la nostra Politica sulla Privacy prima di utilizzare il nostro Servizio.';
+
+$other_websites = 'Link ad Altri Siti Web';
+$other_websites_text1 = 'Il nostro Servizio può contenere collegamenti a siti web o servizi di terze parti che non sono di proprietà o controllati dall\'Azienda.';
+$other_websites_text2 = 'L\'Azienda non ha alcun controllo su tali siti web o servizi di terze parti e non assume alcuna responsabilità riguardo ai contenuti, alle politiche sulla privacy o alle pratiche di tali siti web o servizi di terze parti. Inoltre, riconosci e accetti che l\'Azienda non sarà responsabile, direttamente o indirettamente, per eventuali danni o perdite causate o presunte essere state causate dall\'utilizzo o dalla fiducia in qualsiasi contenuto, bene o servizio disponibile su o tramite tali siti web o servizi di terze parti.';
+$other_websites_text3 = 'Ti consigliamo vivamente di leggere i termini e le condizioni e le politiche sulla privacy di qualsiasi sito web o servizio di terze parti che visiti.';
+
+$termination = 'Terminazione';
+$termination_text1 = 'Possiamo terminare o sospendere immediatamente il tuo accesso, senza preavviso o responsabilità, per qualsiasi motivo, compreso, ma non limitato, alla violazione di questi Termini e Condizioni.';
+$termination_text2 = 'A seguito della terminazione, il tuo diritto di utilizzare il Servizio cesserà immediatamente.';
+
+$liability = 'Limitazione di responsabilità';
+$liability_text1 = 'Nonostante qualsiasi danno che potresti subire, l\'intera responsabilità dell\'Azienda e dei suoi fornitori ai sensi di qualsiasi disposizione di questi Termini e la tua ricorso esclusivo per tutto quanto precede sarà limitata all\'importo effettivamente pagato da te tramite il Servizio o 100 USD se non hai acquistato nulla tramite il Servizio.';
+$liability_text2 = 'Nella massima misura consentita dalla legge applicabile, in nessun caso l\'Azienda o i suoi fornitori saranno responsabili per danni speciali, accidentali, indiretti o consequenziali (incluse, ma non limitate a, perdite di profitti, perdita di dati o altre informazioni, interruzione del business, lesioni personali, perdita di privacy derivante dall\'utilizzo o dall\'impossibilità di utilizzare il Servizio, software di terze parti e/o hardware di terze parti utilizzati con il Servizio, o in relazione a qualsiasi disposizione di questi Termini), anche se l\'Azienda o qualsiasi fornitore è stato informato della possibilità di tali danni e anche se il rimedio non raggiunge lo scopo essenziale.';
+$liability_text3 = 'In alcuni stati non è consentita l\'esclusione di garanzie implicite o la limitazione della responsabilità per danni incidentali o consequenziali, il che significa che alcune delle limitazioni sopra indicate potrebbero non essere applicabili. In questi stati, la responsabilità di ciascuna parte sarà limitata nella misura massima consentita dalla legge.';
+
+$disclaimer = 'Esclusione di garanzie "COME SONO" e "COME DISPONIBILE"';
+$disclaimer_text1 = 'Il Servizio ti viene fornito "COME SONO" e "COME DISPONIBILE", con tutti i difetti e difetti senza garanzia di alcun tipo. Nella misura massima consentita dalla legge applicabile, l\'Azienda, a proprio nome e per conto delle sue affiliate e dei rispettivi licenzianti e fornitori di servizi, esclude espressamente tutte le garanzie, sia esplicite, implicite, statutarie o di altro tipo, riguardanti il Servizio, incluse tutte le garanzie implicite di commerciabilità, idoneità a uno scopo particolare, titolo e non violazione, e garanzie che possono derivare dal corso del commercio, dall\'esecuzione, dall\'uso o dalle prassi commerciali. A titolo esemplificativo, l\'Azienda non fornisce alcuna garanzia o impegno e non rilascia alcuna rappresentazione di alcun tipo che il Servizio soddisferà i tuoi requisiti, raggiungerà alcun risultato desiderato, sarà compatibile o funzionerà con qualsiasi altro software, applicazione, sistema o servizio, funzionerà senza interruzioni, soddisferà standard di prestazioni o affidabilità o sarà privo di errori o che eventuali errori o difetti potranno o saranno corretti.';
+$disclaimer_text2 = 'Senza limitare quanto sopra, né l\'Azienda né alcun fornitore dell\'Azienda fornisce alcuna rappresentazione o garanzia di alcun tipo, esplicita o implicita: (i) riguardo al funzionamento o alla disponibilità del Servizio o delle informazioni, contenuti e materiali o prodotti inclusi in esso; (ii) che il Servizio sarà ininterrotto o privo di errori; (iii) riguardo all\'accuratezza, affidabilità o attualità di qualsiasi informazione o contenuto fornito tramite il Servizio; o (iv) che il Servizio, i suoi server, i contenuti o le e-mail inviate da parte dell\'Azienda o a suo nome sono privi di virus, script, cavalli di Troia, worm, malware, bombe a orologeria o altri componenti dannosi.';
+$disclaimer_text3 = 'In alcune giurisdizioni non è consentita l\'esclusione di determinati tipi di garanzie o limitazioni dei diritti legali applicabili a un consumatore, pertanto alcune o tutte le esclusioni e limitazioni sopra indicate potrebbero non essere applicabili a te. In tal caso, le esclusioni e limitazioni stabilite in questa sezione saranno applicate nella massima misura consentita dalla legge applicabile.';
+
+$law = 'Legge Applicabile';
+$law_text = 'Le leggi del Paese, escludendo le regole di conflitto di leggi, disciplineranno questi Termini e il tuo utilizzo del Servizio. Il tuo utilizzo dell\'Applicazione potrebbe anche essere soggetto ad altre leggi locali, statali, nazionali o internazionali.';
+
+$resolution = 'Risoluzione delle Controversie';
+$resolution_text = 'Se hai qualsiasi preoccupazione o controversia riguardo al Servizio, accetti di cercare prima di risolvere la controversia informalmente contattando l\'Azienda.';
+
+$eu_users = 'Per gli utenti dell\'Unione Europea (UE)';
+$eu_users_text = 'Se sei un consumatore dell\'Unione Europea, beneficerai delle disposizioni obbligatorie della legge del paese in cui risiedi.';
+
+$usa_users = 'Conformità Legale degli Utenti degli Stati Uniti';
+$usa_users_text = 'Dichiari e garantisci che (i) non ti trovi in un paese soggetto all\'embargo del governo degli Stati Uniti o che è stato designato dal governo degli Stati Uniti come un paese che "supporta il terrorismo" e (ii) non sei incluso in nessuna lista del governo degli Stati Uniti di parti proibite o limitate.';
+$severability_waiver = 'Separabilità e Rinuncia';
+$severability = 'Separabilità';
+$severability_text = 'Se una qualsiasi disposizione di questi Termini viene ritenuta inapplicabile o invalida, tale disposizione sarà modificata e interpretata per raggiungere gli obiettivi di tale disposizione nella misura massima possibile ai sensi della legge applicabile e le disposizioni restanti continueranno ad essere in pieno vigore ed effetto.';
+$waiver = 'Rinuncia';
+$waiver_text = 'Salvo quanto previsto in questo documento, il mancato esercizio di un diritto o la richiesta di ottemperanza di un obbligo ai sensi di questi Termini non pregiudicherà la capacità di una parte di esercitare tale diritto o richiedere tale ottemperanza in qualsiasi momento successivo, né la rinuncia a una violazione costituirà una rinuncia a qualsiasi violazione successiva.';
+
+$translation = 'Interpretazione delle traduzioni';
+$translation_text = 'Questi Termini e Condizioni possono essere stati tradotti, se li abbiamo resi disponibili sul nostro Servizio. Accetti che il testo originale in inglese prevarrà in caso di controversia.';
+
+$changes = 'Modifiche a questi Termini e Condizioni';
+$changes_text1 = 'Ci riserviamo il diritto, a nostra esclusiva discrezione, di modificare o sostituire questi Termini in qualsiasi momento. Se una revisione è materiale, faremo ragionevoli sforzi per fornire almeno 30 giorni di preavviso prima che le nuove condizioni entrino in vigore. Cosa costituisce una modifica materiale sarà determinato a nostra esclusiva discrezione.';
+$changes_text2 = 'Continuando ad accedere o utilizzare il nostro Servizio dopo che tali revisioni sono entrate in vigore, accetti di essere vincolato dai termini rivisti. Se non accetti i nuovi termini, in tutto o in parte, ti preghiamo di interrompere l\'utilizzo del sito web e del Servizio.';
+
+$contact = 'Contattaci';
+$contact_text = 'Se hai domande su questi Termini e Condizioni, puoi contattarci:';
+$contact_email = 'Via email: codeliftoffteam@gmail.com';
+$contact_website = 'Visitando questa pagina sul nostro sito web: <a href="www.codeliftoff.com/pages/terms.php" rel="external nofollow noopener" target="_blank">www.codeliftoff.com/pages/terms.php</a>';
+
+?>
